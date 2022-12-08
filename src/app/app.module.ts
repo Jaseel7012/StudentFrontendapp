6af77dae151529eb23c82e10762ec7b90414c2b4
jaseel <jaseeljctcs@gmail.com>
2022-12-08 15:16:44 +0530

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {  RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentAddComponent } from './student-add/student-add.component';
 
+const myroute:Routes=[
+  {
+    path:'',
+    component:StudentAddComponent
+    
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentAddComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myroute)
   ],
   providers: [],
   bootstrap: [AppComponent]
